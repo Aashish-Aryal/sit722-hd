679f26b57baa5be0f634204959742e5eb697974e
Aashish-Aryal <mail.aashisharyal@gmail.com>
2024-10-04 06:34:59 +1000

    Run CD #3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,12 +11,11 @@ sed -i 's/\$APP_NAME/'$APP_NAME'/g' ./scripts/kubernetes/deployment.yaml
 sed -i 's/\$VERSION/'$VERSION'/g' ./scripts/kubernetes/deployment.yaml
 
 # Create Kubernetes secrets using GitHub Secrets
-kubectl create secret generic app-secrets \
-          --from-literal=mongo_url=$MONGO_URL }} \
-          --from-literal=jwt_secret=$JWT_SECRET }} \
-          --from-literal=cloud_name=$CLOUD_NAME }} \
-          --from-literal=cloud_api_key=$CLOUD_API_KEY }} \
-          --from-literal=cloud_api_secret=$CLOUD_API_SECRET }}
+kubectl create secret generic app-secrets --from-literal=mongo_url=$MONGO_URL
+kubectl create secret generic app-secrets --from-literal=jwt_secret=$JWT_SECRET
+kubectl create secret generic app-secrets --from-literal=cloud_name=$CLOUD_NAME
+kubectl create secret generic app-secrets --from-literal=cloud_api_key=$CLOUD_API_KEY
+kubectl create secret generic app-secrets --from-literal=cloud_api_secret=$CLOUD_API_SECRET
 
 # Apply the Kubernetes deployment
 kubectl apply -f ./scripts/kubernetes/deployment.yaml
